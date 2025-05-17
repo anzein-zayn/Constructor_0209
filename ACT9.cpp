1a785e0f97642cb021ff941506230ae9f5d3d775
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-05-17 15:50:51 +0700

    Membuat class buku

--- ACT9.cpp
+++ ACT9.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 #include <string>
+
+class Buku
+{
+private :
+    string judul;
+    string penulis;
+    string statuspinjam;
+public:
+    void setName(string pNama);
+}
